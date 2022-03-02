87bbbf9582b4f3499dd4efa2d0f54d3fa185e7a1
MADEYE <52187012+seyi91@users.noreply.github.com>
2022-03-01 20:41:05 -0600

    Add addKeyAndValue()

--- reduce.js
+++ reduce.js
@@ -43,7 +43,8 @@ function vowelCount(str) {
 }
 
 /*
-Write a function called addKeyAndValue which accepts an array of objects and returns the array of objects passed to it with each object now including the key and value passed to the function.
+Write a function called addKeyAndValue which accepts an array of objects and returns the array of objects passed to it 
+with each object now including the key and value passed to the function.
 
 Examples:
     const arr = [{name: 'Elie'}, {name: 'Tim'}, {name: 'Matt'}, {name: 'Colt'}];
@@ -57,7 +58,13 @@ Examples:
        ]
 */
 
-function addKeyAndValue(arr, key, value) {}
+function addKeyAndValue(arr, key, value) {
+    return arr.reduce(function(acc, next, idx){
+        acc[idx][key] = value;
+        return acc;
+    }, arr);
+    
+}
 
 /*
 Write a function called partition which accepts an array and a callback and returns an array with two arrays inside of it. The partition function should run the callback function on each value in the array and if the result of the callback function at that specific value is true, the value should be placed in the first subarray. If the result of the callback function at that specific value is false, the value should be placed in the second subarray. 
