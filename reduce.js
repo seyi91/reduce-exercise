89e2599d6f81cfdbbb3c1b1019612f50e27c0323
MADEYE <52187012+seyi91@users.noreply.github.com>
2022-03-01 20:24:28 -0600

    Add vowelCount()

--- reduce.js
+++ reduce.js
@@ -15,7 +15,9 @@ function extractValue(arr, key) {
 };
 
 /*
-Write a function called vowelCount which accepts a string and returns an object with the keys as the vowel and the values as the number of times the vowel appears in the string. This function should be case insensitive so a lowercase letter and uppercase letter should count
+Write a function called vowelCount which accepts a string and returns an object with the keys as the vowel and the values 
+as the number of times the vowel appears in the string. This function should be case insensitive so a lowercase letter 
+and uppercase letter should count
 
 Examples:
     vowelCount('Elie') // {e:2,i:1};
@@ -25,7 +27,20 @@ Examples:
     vowelCount('I Am awesome and so are you') // {i: 1, a: 4, e: 3, o: 3, u: 1};
 */
 
-function vowelCount(str) {}
+function vowelCount(str) {
+    const vowels = 'aeiou';
+    return str.split('').reduce(function(acc, next) {
+        let lowerCased = next.toLowerCase();
+        if (vowels.indexOf(lowerCased) !== -1) {
+            if (acc[lowerCased]) {
+                acc[lowerCased]++;
+            } else {
+                acc[lowerCased] = 1;
+            }
+        }
+        return acc;
+    }, {})
+}
 
 /*
 Write a function called addKeyAndValue which accepts an array of objects and returns the array of objects passed to it with each object now including the key and value passed to the function.
