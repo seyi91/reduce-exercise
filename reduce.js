2b8f26687a639a19f5c7851ea034ad03b5764cb6
MADEYE <52187012+seyi91@users.noreply.github.com>
2022-03-01 20:59:16 -0600

    add partition()

--- reduce.js
+++ reduce.js
@@ -67,7 +67,10 @@ function addKeyAndValue(arr, key, value) {
 }
 
 /*
-Write a function called partition which accepts an array and a callback and returns an array with two arrays inside of it. The partition function should run the callback function on each value in the array and if the result of the callback function at that specific value is true, the value should be placed in the first subarray. If the result of the callback function at that specific value is false, the value should be placed in the second subarray. 
+Write a function called partition which accepts an array and a callback and returns an array with two arrays inside of 
+it. The partition function should run the callback function on each value in the array and if the result of the 
+callback function at that specific value is true, the value should be placed in the first subarray. If the result of the 
+callback function at that specific value is false, the value should be placed in the second subarray. 
 
 Examples:
     
@@ -88,4 +91,13 @@ Examples:
     partition(names, isLongerThanThreeCharacters) // [['Elie', 'Colt', 'Matt'], ['Tim']]
 */
 
-function partition(arr, callback) {}
+function partition(arr, callback) {
+    return arr.reduce(function(acc, next){
+        if (callback(next)){
+            acc[0].push(next);
+        } else {
+            acc[1].push(next);
+        }
+        return acc;
+    }, [[], []])
+}
